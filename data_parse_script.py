0b7ff21d5c5279c0b5b79c7fe304543e540ab185
Ujjwal Tamhankar <utamhank1@gmail.com>
2020-02-11 10:42:50 -0500

    Commit to rename case data to one word no spaces, convert to .csv, and set up architecture for argparser for data_parse_script.py

---
+++ data_parse_script.py
@@ -0,0 +1,26 @@
+"""This script takes in the arguments for the path of the users local copy of "Spring 2018 - Product Case Data.xlsx"
+which, for the sake of running in a linux terminal, we rename to "spring_2018_product_case_data.csv" and parses out
+metrics such as the conversion rate, bounce rate and necessary graphs as described in parts 1-5 of part 1 of the case
+"""
+
+# Import necessary libraries
+import argparse
+import pandas as pd
+import numpy as np
+
+
+def parser_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--path", help="path to your copy of spring_2018_product_case_data.csv")
+    args = parser.parse_args()
+    return args
+
+
+def main(args):
+    path = args.path
+    print(f"path = {path}")
+
+
+if __name__ == "__main__":
+    arguments = parser_arguments()
+    main(arguments)
